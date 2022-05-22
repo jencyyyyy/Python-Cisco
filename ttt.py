323ac5219a59325037a75f1ee822dc4bcdba2ff3
Teresa Jency Bala <jency.cse2k18@gmail.com>
2022-05-22 12:08:11 +0600

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -1,93 +1,90 @@
-# 4.1.6.13 PROJECT: Tic-Tac-Toe
-from random import randrange
+# 4.1.6.13 PROJECT: Tic-Tac-Toefrom random import randrange
 
-def DisplayBoard(board):
-	print("+-------" * 3,"+",sep="")
-	for row in range(3):
-		print("|       " * 3,"|",sep="")
-		for col in range(3):
-			print("|   " + str(board[row][col]) + "   ",end="")
-		print("|")
-		print("|       " * 3,"|",sep="")
-		print("+-------" * 3,"+",sep="")
+print("-------Tic-Tac-Toe-------")
+#Updates the user my printing the board to the console
+def display_board(board):
+    #print(board)
+    for i in range(0,len(board),3):
+        print("+-------"*3 + "+")
+        print("|       "*3 + "|")
+        print("|   " + str(board[i]) + \
+                 "   |   " + str(board[i + 1]) + \
+                "   |   " + str(board[i + 2]) + "   |   ")
+        print("|       "*3 + "|")
+    print("+-------"*3 + "+")
 
+
+
+# This function takes the users input and updates the board 
 def EnterMove(board):
-	ok = False	# fake assumption - we need it to enter the loop
-	while not ok:
-		move = input("Enter your move: ") 
-		ok = len(move) == 1 and move >= '1' and move <= '9' # is user's input valid?
-		if not ok:
-			print("Bad move - repeat your input!") # no, it isn't - do the input again
-			continue
-		move = int(move) - 1 	# cell's number from 0 to 8
-		row = move // 3 	# cell's row
-		col = move % 3		# cell's column
-		sign = board[row][col]	# check the selected square
-		ok = sign not in ['O','X'] 
-		if not ok:	# it's occupied - to the input again
-			print("Field already occupied - repeat your input!")
-			continue
-	board[row][col] = 'O' 	# set '0' at the selected square
+    FreeSpace = MakeListOfFreeSpace(board)
+    position = int(input("Your turn:"))
 
-def MakeListOfFreeFields(board):
-	free = []	# the list is empty initially
-	for row in range(3): # iterate through rows
-		for col in range(3): # iterate through columns
-			if board[row][col] not in ['O','X']: # is the cell free?
-				free.append((row,col)) # yes, it is - append new tuple to the list
-	return free
+    while(position < 1 or position > 9):
+        print("Please enter number 1-9")
+        position = int(input("Your turn:"))
+        
+    while True:
+        if position in FreeSpace:
+            board[position - 1] = 'O'
+            break
+        else:
+            print('That spot has been taken')
+            position = int(input("Your turn:"))
+    display_board(board)
+
+# This function checks to see if there are any free spaces and makes a list of the ones avalable
+def MakeListOfFreeSpace(board):
+    FreeSpace = []
+    for i in range(len(board)):
+            if board[i] != 'O' and board[i]!='X':
+                FreeSpace.append(board[i])
+    return FreeSpace
 
 
-def VictoryFor(board,sgn):
-	if sgn == "X":	# are we looking for X?
-		who = 'me'	# yes - it's computer's side
-	elif sgn == "O": # ... or for O?
-		who = 'you'	# yes - it's our side
-	else:
-		who = None	# we should not fall here!
-	cross1 = cross2 = True  # for diagonals
-	for rc in range(3):
-		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:	# check row rc
-			return who
-		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn: # check column rc
-			return who
-		if board[rc][rc] != sgn: # check 1st diagonal
-			cross1 = False
-		if board[2 - rc][2 - rc] != sgn: # check 2nd diagonal
-			cross2 = False
-	if cross1 or cross2:
-		return who
-	return None
+#Checks to see if the O or X player has won the game
+def Victory(board, sign):
+    arrangements = [[1, 2, 3], [4, 5, 6], [7, 8, 9],[1, 4, 7],  [2, 5, 8], [3, 6, 9],[1, 5, 9], [3, 5, 7]]
+    for i in range(len(arrangements)):
+        position1, position2, position3 = arrangements[i][0] - 1, arrangements[i][1] - 1, arrangements[i][2] - 1
+        if (str(board[position1]) == sign) and (str(board[position2]) == sign) and (str(board[position3]) == sign):
+            return True
+    return False
 
-def DrawMove(board):
-	free = MakeListOfFreeFields(board) # make a list of free fields
-	cnt = len(free)
-	if cnt > 0:	# if the list is not empty, choose a place for 'X' and set it
-		this = randrange(cnt)
-		row, col = free[this]
-		board[row][col] = 'X'
 
-board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # make an empty board
-board[1][1] = 'X' # set first 'X' in the middle
-free = MakeListOfFreeFields(board)
-humanturn = True # which turn is it now?
-while len(free):
-	DisplayBoard(board)
-	if humanturn:
-		EnterMove(board)
-		victor = VictoryFor(board,'O')
-	else:	
-		DrawMove(board)
-		victor = VictoryFor(board,'X')
-	if victor != None:
-		break
-	humanturn = not humanturn		
-	free = MakeListOfFreeFields(board)
+# This function is how the computer makes the move and it updates the board
+def draw_move(board):
+    FreeSpace = MakeListOfFreeSpace(board)
+    position = randrange(1,10)
+    while True:
+        if position in FreeSpace:
+            board[position - 1] = 'X'
+            break
+        else:
+            position = randrange(1, 10)
+    display_board(board)
 
-DisplayBoard(board)
-if victor == 'you':
-	print("You won!")
-elif victor == 'me':
-	print("I won")
-else:
-	print("Tie!")
+#This is the list for the board
+board = [1, 2, 3, 4, 'X', 6, 7, 8, 9]
+
+display_board(board)
+while True:
+#checks to see if the board is filled
+    moves = 0
+    for i in range(len(board)):
+        if board[i] == 'O' or board[i] =='X':
+            moves +=1
+    if moves == len(board):
+        print('Game is Tie')
+        break
+
+#When its the users turn 
+    EnterMove(board)
+    if Victory(board, 'O'):
+        print('You won!!!')
+        break
+# When its the computers 
+    draw_move(board)
+    if Victory(board, 'X'):
+        print('Computer won!!!')
+        break
